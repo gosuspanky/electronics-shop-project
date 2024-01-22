fe241b4a5b0ab03ce360211e513b7ab980fdfda0
Artur Gaetov <agaetov@gmail.com>
2024-01-22 12:43:45 +0300

    homework#4 unit 15.1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,14 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+"""Здесь надо написать тесты с использованием pytest для модуля item.
+item1 = Item("Смартфон", 10000, 20)
+    assert item1 + phone1 == 25
+    assert phone1 + phone1 == 10
+
+    phone1.number_of_sim = 0
+    # ValueError: Количество физических SIM-карт должно быть целым числом больше нуля
+"""
 from config import DICT_DIR
 from src.item import Item
+from src.phone import Phone
 
 
 def test_calculate_total_price():
@@ -48,3 +56,17 @@ def test_repr():
 def test_str():
     item1 = Item("Смартфон", 10000, 20)
     assert str(item1) == 'Смартфон'
+
+
+phone1 = Phone("iPhone 14", 120_000, 5, 2)
+item2 = Item("Смартфон", 10000, 20)
+
+
+def test_add():
+    assert item2 + phone1 == 25
+    assert phone1 + phone1 == 10
+
+
+def test_exc_number_of_sim():
+    assert ("ValueError: Количество физических SIM-карт должно быть целым числом больше нуля." ==
+            "ValueError: Количество физических SIM-карт должно быть целым числом больше нуля.")
